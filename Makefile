e23793b1ea0ab8c8ec6c99379be73b13397d999f
Andrea Agosti <cifvts@gmail.com>
2010-12-31 14:17:43 +0100

    Add solution for problem 3

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ BIN=./bin
 
 CFLAGS=-O2
 
-main: 001 002
+main: 001 002 003
 
 %.dir:
 	test -d $(BIN) || mkdir -p $(BIN)
@@ -13,3 +13,6 @@ main: 001 002
 
 002: %.dir
 	gcc $(CFLAGS) $(SRC)/euler002.c -o $(BIN)/euler002
+
+003: %.dir
+	gcc $(CFLAGS) $(SRC)/euler003.c -o $(BIN)/euler003
---
+++ src/euler003.c
@@ -0,0 +1,55 @@
+/* Copyright (c) 2009 Andrea Agosti
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* Project Euler - http://projecteuler.net
+ * Problem 3
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define TARGET 600851475143
+
+int main()
+{
+  uint64_t dividend = TARGET;
+  uint32_t factor;
+
+  /* This is optimized for the odd TARGET */ 
+  for (factor = 3; dividend > 1; factor += 2)
+    {
+      while (dividend % factor == 0)
+        {
+          dividend /= factor;
+        }
+    }
+
+  printf ("%d\n", factor - 2);
+
+  return 0;
+}
